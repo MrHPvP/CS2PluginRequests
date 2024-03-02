5147a3025258bfaa6c8b86d7244fe833f5574649
MrHPvP <46511195+MrHPvP@users.noreply.github.com>
2024-03-02 20:26:50 +1000

    added returns thanks schwarper

--- WardenTimer/WardenTimer.cs
+++ WardenTimer/WardenTimer.cs
@@ -14,16 +14,18 @@ public partial class WardenTimer : BasePlugin
 	public override string ModuleAuthor => "MrH_PvP";
 	public override string ModuleVersion => "1.0.0";
 	Timer timewillcatchuptousoneday;
+	bool timeractive = false;
 
 	[GameEventHandler]
 	public HookResult whenroundhasreacheditslimitforplayingwiththesepeople(EventRoundEnd @event, GameEventInfo info)
 	{
 		timewillcatchuptousoneday.Kill();
+		timeractive = false;
 		return HookResult.Continue;
 	}
 
 	[ConsoleCommand("css_time", "creates a timer so the warden knows how long he can ramble on for")]
-	[CommandHelper(1, "timernumbers", CommandUsage.CLIENT_AND_SERVER)]
+	[CommandHelper(1, "Time", CommandUsage.CLIENT_AND_SERVER)]
 	public void getthisterriblewardenoutoffheretimer(CCSPlayerController? caller, CommandInfo args)
 	{
 		var timeywhymy = 0;
@@ -31,13 +33,20 @@ public partial class WardenTimer : BasePlugin
 		if (!caller.PlayerName.Contains("Warden"))
 		{
 			args.ReplyToCommand($"come back when your the warden pleb.");
+			return;
 		}
 		if (!int.TryParse(args.ArgByIndex(1), out timeywhymy))
 		{
 			args.ReplyToCommand($"You need to specify a number");
+			return;
+		}
+		if(timeractive){
+			args.ReplyToCommand($"A timer is already active");
+			return;
 		}
 		timewillcatchuptousoneday = AddTimer(1f, () =>
 		{
+			timeractive = true;
 			var allplayers = Utilities.GetPlayers();
 			timeywhymy -= 1;
 			foreach (var player in allplayers)
@@ -47,6 +56,7 @@ public partial class WardenTimer : BasePlugin
 				player.PrintToCenter($"you have {time} left");
 				if (timeywhymy < 1)
 				{
+					timeractive = false;
 					player.PrintToCenter("The Wardens timer has ended!");
 					timewillcatchuptousoneday.Kill();
 				}
